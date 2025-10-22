ca0bb06e56afe1f2b7140bf3753c8e7e90ff1d5e
invalidusernamecalled <puneetbapna@gmail.com>
2025-10-22 05:36:17 +0530

    Removed debug message

--- random_range.bat
+++ random_range.bat
@@ -14,11 +14,12 @@ goto :eof
 :continue
 echo %first_arg% %second_arg%|findstr /rc:"^[0-9][0-9]* [0-9][0-9]*$" >NUL&&echo:>NUL || goto :syntaxerror
 
-if %second_arg% LSS %first_arg% set /a result=(%RANDOM%*(first_arg-second_arg)/32768)+second_arg&echo HERE
+if %second_arg% LSS %first_arg% set /a result=(%RANDOM%*(first_arg-second_arg)/32768)+second_arg
 if %second_arg% GEQ %first_arg% set /a result=(%RANDOM%*(second_arg-first_arg)/32768)+first_arg
 echo %RESULT%
 goto :eof
 
 :syntaxerror
 echo:"%~nx0" [number1] [number2]
+
 echo: Eg. "%~nx0" 2 4
